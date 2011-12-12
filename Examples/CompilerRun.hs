792d7515823e76db23e1de4370d715026f153e33
Masayuki Takagi <kamonama@gmail.com>
2011-12-12 21:23:24 +0900

    * exported printQ function in Compiler/Codegen.hs, for easy debugging * modified to be able to specify the compiler's output variables as an argument of compile function

--- Examples/CompilerRun.hs
+++ Examples/CompilerRun.hs
@@ -6,5 +6,4 @@ import qualified Data.Vector as V
 
 -- main = print $ take 10 $ $(compile machine19)
 
-main = print $ map (V.toList . (\(v,x,f,a) -> x))
-             $ take 100 $ $(compile machine19)
+main = print $ take 100 $ $(compile ["x"] machine19)
